6e700fb4f0e255cd2cb3979afa95bfe9e29f4120
SusuMantan <suzumantan@gmail.com>
2022-04-25 11:11:59 +0700

    compgen and show info

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,127 +2,184 @@
 
 set -o pipefail
 
+# UTILITY
+shw_white () {
+  echo $(tput bold)$(tput setaf 7)"$@"$(tput sgr 0)
+}
+
+shw_norm () {
+  echo $(tput setaf 0)"$@"$(tput sgr 0)
+}
+
+shw_info () {
+  echo $(tput bold)$(tput setaf 4)"INFO:: $@"$(tput sgr 0)
+}
+
+shw_warn () {
+  echo $(tput bold)$(tput setaf 3)"WARN:: $@"$(tput sgr 0)
+}
+shw_err ()  {
+  echo $(tput bold)$(tput setaf 1)"ERROR:: $@"$(tput sgr 0)
+}
+
+prn_header() {
+  echo
+  shw_white "$1"
+  echo "---------------------"
+}
+
+prn_footer() {
+  echo "---------------------"
+}
+# END UTILITY
+
 install_ohmyzsh() {
+  prn_header "install oh-my-zsh"
   if [ ! -d $HOME/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
   else
-    echo "Skip oh-my-zsh"
+    shw_warn "oh-my-zsh is already installed"
   fi
+  prn_footer
 }
 
 install_powerlevel10k() {
+  prn_header "install powerlevel10k"
   if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
   else
+    shw_warn "powerlevel10k is already installed"
+    echo
+    shw_norm "Updating powerlevel10k" 
     git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k pull
   fi
+  prn_footer
 }
 
 install_fzf() {
+  prn_header "install fzf"
   TARGET_DIR="$HOME/.fzf"
   if [ ! -d $HOME/.fzf ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git "$TARGET_DIR"
   else
+    shw_warn "fzf is already installed"
+    echo
+    shw_norm "Updating fzf" 
     git -C "$TARGET_DIR" pull
   fi
 
-  ~/.fzf/install --completion --key-bindings --no-update-rc
+  ~/.fzf/install --completion --key-bindings --no-update-rc > /dev/null 2>&1
+  prn_footer
 }
 
 install_fnm() {
+  prn_header "install fnm"
   curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
+  prn_footer
 }
 
 install_docker() {
+  prn_header "install docker"
   if ! command -v docker &> /dev/null; then
     curl -fsSL https://get.docker.com | sh
   else
-    echo "Skip docker"
+    shw_warn "docker is already installed"
   fi
+  prn_footer
 }
 
 install_nnn() {
+  prn_header "install nnn"
   local TARGET_BIN_DIR="$HOME/bin"
   local D_URL="https://api.github.com/repos/jarun/nnn/releases/latest"
 
-  if find $TARGET_BIN_DIR -name 'nnn*'; then
-    echo "Found nnn in $TARGET_BIN_DIR. Proceed to removing..."
-    rm -v $TARGET_BIN_DIR/nnn*
+  shw_norm "Checking binaries in $TARGET_BIN_DIR"
+  if compgen -G "$TARGET_BIN_DIR/nnn*" > /dev/null; then
+    shw_info "Found binary in $TARGET_BIN_DIR. Proceed to removing..."
+    rm $TARGET_BIN_DIR/nnn*
   fi
+  echo
 
   TEMP_DIR="$(mktemp -d)"
   pushd $TEMP_DIR > /dev/null
-  echo "Downloading nnn in $PWD"
 
+  shw_norm "Download and install"
   if ! curl -s $D_URL | grep -wo 'https.*nnn-static*.*tar.gz' | wget -qi -; then
-    echo "Failed to download nnn"
+    shw_err "Failed to download"
   else
     tar xzf nnn-static-*
     mv nnn-static nnn
     mv nnn $TARGET_BIN_DIR
     chmod +x $TARGET_BIN_DIR/nnn
-    echo "nnn installed in $TARGET_BIN_DIR"
+    shw_info "Installed in $TARGET_BIN_DIR"
   fi
   popd > /dev/null
 
-  echo "Cleaning up $TEMP_DIR"
-  rm -rv $TEMP_DIR
+  rm -r $TEMP_DIR
+  prn_footer
 }
 
 install_nvim() {
+  prn_header "install nvim"
   local TARGET_BIN_DIR="$HOME/bin"
   local D_URL="https://api.github.com/repos/neovim/neovim/releases/latest"
 
-  if find $TARGET_BIN_DIR -name 'nvim*'; then
-    echo "Found nvim in $TARGET_BIN_DIR. Proceed to removing..."
-    rm -v $TARGET_BIN_DIR/nvim*
+  shw_norm "Checking binaries in $TARGET_BIN_DIR"
+  if compgen -G "$TARGET_BIN_DIR/nvim*" > /dev/null; then
+    shw_info "Found binary in $TARGET_BIN_DIR. Proceed to removing..."
+    rm $TARGET_BIN_DIR/nvim*
   fi
+  echo
 
   TEMP_DIR="$(mktemp -d)"
   pushd $TEMP_DIR > /dev/null
-  echo "Downloading nvim in $PWD"
 
+  shw_norm "Download and install"
   if ! curl -s $D_URL | grep -wo '"https.*nvim\.appimage"' | tr -d '"' | wget -qi -; then
-    echo "Failed to download nvim"
+    shw_err "Failed to download"
   else
     mv nvim* $TARGET_BIN_DIR/nvim
     chmod +x $TARGET_BIN_DIR/nvim
-    echo "nvim installed in $TARGET_BIN_DIR"
+    shw_info "Installed in $TARGET_BIN_DIR"
   fi
   popd > /dev/null
 
-  echo "Cleaning up $TEMP_DIR"
-  rm -rv $TEMP_DIR
+  rm -r $TEMP_DIR
+  prn_footer
 }
 
 install_7z() {
+  prn_header "install 7z"
   local TARGET_BIN_DIR="$HOME/bin"
   local D_URL="https://www.7-zip.org/a/7z2107-linux-x64.tar.xz"
 
-  if find $TARGET_BIN_DIR -name '7z*'; then
-    echo "Found 7z in $TARGET_BIN_DIR. Proceed to removing..."
-    rm -v $TARGET_BIN_DIR/7z*
+  shw_norm "Checking binaries in $TARGET_BIN_DIR"
+  if compgen -G "$TARGET_BIN_DIR/7z*" > /dev/null; then
+    shw_info "Found binary in $TARGET_BIN_DIR. Proceed to removing..."
+    rm $TARGET_BIN_DIR/7z*
   fi
+  echo
 
   TEMP_DIR="$(mktemp -d)"
   pushd $TEMP_DIR > /dev/null
-  echo "Downloading 7z in $PWD"
-
+  
+  shw_norm "Download and install"
   if ! wget -q $D_URL; then
-    echo "Failed to download 7z"
+    shw_err "Failed to download 7z"
   else
     tar xf 7z2107-linux-x64.tar.xz
     mv 7zzs $TARGET_BIN_DIR/7z
     chmod +x $TARGET_BIN_DIR/7z
-    echo "7z installed in $TARGET_BIN_DIR"
+    shw_info "Installed in $TARGET_BIN_DIR"
   fi
   popd > /dev/null
 
-  echo "Cleaning up $TEMP_DIR"
-  rm -rv $TEMP_DIR
+  rm -r $TEMP_DIR
+  prn_footer
 }
 
 install_docker_compose() {
+  prn_header "install docker-compose"
   DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
   local TARGET_BIN_DIR="$DOCKER_CONFIG/cli-plugins"
   if [ ! -d $TARGET_BIN_DIR ]; then
@@ -131,26 +188,28 @@ install_docker_compose() {
 
   local D_URL="https://api.github.com/repos/docker/compose/releases/latest"
 
-  if find $TARGET_BIN_DIR -name 'docker-compose*'; then
-    echo "Found compose in $TARGET_BIN_DIR. Proceed to removing..."
-    rm -v $TARGET_BIN_DIR/docker-compose*
+  shw_norm "Checking binaries in $TARGET_BIN_DIR"
+  if compgen -G "$TARGET_BIN_DIR/docker-compose*" > /dev/null; then
+    shw_info "Found binary in $TARGET_BIN_DIR. Proceed to removing..."
+    rm $TARGET_BIN_DIR/docker-compose*
   fi
+  echo
 
   TEMP_DIR="$(mktemp -d)"
   pushd $TEMP_DIR > /dev/null
-  echo "Downloading compose in $PWD"
 
+  shw_norm "Downloading and install"
   if ! curl -s $D_URL | grep -wo '"https.*linux-x86_64"' | tr -d '"' | wget -qi -; then
-    echo "Failed to download compose"
+    shw_err "Failed to download"
   else
     mv docker-compose* $TARGET_BIN_DIR/docker-compose
     chmod +x $TARGET_BIN_DIR/docker-compose
-    echo "compose installed in $TARGET_BIN_DIR"
+    shw_info "Installed in $TARGET_BIN_DIR"
   fi
   popd > /dev/null
 
-  echo "Cleaning up $TEMP_DIR"
-  rm -rv $TEMP_DIR
+  rm -r $TEMP_DIR
+  prn_footer
 }
 
 main() {
@@ -164,18 +223,18 @@ main() {
     esac 
   done
 
-  # install_ohmyzsh
+  install_ohmyzsh
   install_powerlevel10k
   install_fzf
-  # install_fnm
-  # install_docker
-  # install_docker_compose
-  # install_nnn
-  # install_nvim
-  # install_7z
+  install_fnm
+  install_docker
+  install_docker_compose
+  install_nnn
+  install_nvim
+  install_7z
 
   echo
-  echo "Installing done!"
+  shw_white "Installing done!"
 }
 
 main
