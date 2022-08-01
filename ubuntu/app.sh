7553b5647f6f358b1528875eda4adf0c56e30f79
SusuMantan <suzumantan@gmail.com>
2022-08-02 05:20:40 +0700

    update

--- ubuntu/app.sh
+++ ubuntu/app.sh
@@ -3,83 +3,91 @@
 set -o pipefail
 printf "\n"
 
-## 
-extract() {
-  for arg in $@; do
-    if [ -f "$arg" ]; then
-      case $arg in
-        *.tar.bz2)  tar xjf $arg      ;;
-        *.tar.gz)   tar xzf $arg      ;;
-        *.tar.xz)   tar xzf $arg      ;;
-        *.bz2)      bunzip2 $arg      ;;
-        *.gz)       gunzip $arg       ;;
-        *.tar)      tar xf $arg       ;;
-        *.tbz2)     tar xjf $arg      ;;
-        *.tgz)      tar xzf $arg      ;;
-        *.zip)      unzip $arg        ;;
-        *.Z)        uncompress $arg   ;;
-        *.rar)      rar x $arg        ;;  # 'rar' must to be installed
-        *.jar)      jar -xvf $arg     ;;  # 'jdk' must to be installed
-        *)          echo "'$arg' cannot be extracted via extract()" ;;
+# Functions to install packages from github
+# Usage:
+#  install_from_github <command-name> <repo-short-url>
+#
+# Example:
+#  install_from_github fd sharkdp/fd
+install_from_github() {
+  if ! command -v $1 > /dev/null; then
+    local tmp="/tmp/.github-install"
+    local binpath="$HOME/bin"
+
+    rm -rf $tmp
+    mkdir -p $tmp
+    local url="https://api.github.com/repos/$2/releases/latest"
+    echo "Reading... $url"
+
+    PS3="Select download file: "
+    select filename in $(curl -s "$url" | jq -r '.assets[].name'); do break; done
+    echo "Downloading... $filename"
+    curl -s "$url" \
+      | jq -r --arg filename "$filename" '.assets[] | select(.name == $filename) | .browser_download_url' \
+      | wget -i- -q --show-progress -P "$tmp"
+
+    pushd $tmp
+
+    if [ -f "$filename" ]; then
+      case $filename in
+        *.tar.bz2)  tar xjf $filename      ;;
+        *.tar.gz)   tar xzf $filename      ;;
+        *.tar.xz)   tar xzf $filename      ;;
+        *.bz2)      bunzip2 $filename      ;;
+        *.gz)       gunzip $filename       ;;
+        *.tar)      tar xf $filename       ;;
+        *.tbz2)     tar xjf $filename      ;;
+        *.tgz)      tar xzf $filename      ;;
+        *.zip)      unzip $filename        ;;
+        *.Z)        uncompress $filename   ;;
+        *.rar)      rar x $filename        ;;  # 'rar' must to be installed
+        *.jar)      jar -xvf $filename     ;;  # 'jdk' must to be installed
+        *)          echo "'$filename' cannot be extracted via extract()" ;;
       esac
     else
-      echo "'$arg' is not a valid file"
+      echo "'$filename' is not a valid file"
+      exit 1
     fi
-  done
-}
 
-install_from_github() {
-  local tmp="/tmp/.github-install"
-  local binpath="$HOME/bin"
+    PS3="Select binary: "
+    select bin in $(find . -type f); do break; done
+    popd
 
-  rm -rf $tmp
-  mkdir -p $tmp
-  local url="https://api.github.com/repos/$1/releases/latest"
-  echo "Reading... $url"
-
-  PS3="Select download file: "
-  select filename in $(curl -s "$url" | jq -r '.assets[].name'); do break; done
-  echo "Downloading... $filename"
-  curl -s "$url" \
-    | jq -r --arg filename "$filename" '.assets[] | select(.name == $filename) | .browser_download_url' \
-    | wget -i- -q --show-progress -P "$tmp"
-
-  pushd $tmp
-  extract $filename
-  PS3="Select binary: "
-  select bin in $(find . -type f); do break; done
-  popd
-
-  # install
-  basename=$(basename $bin)
-  read -p "Choose an alias (empty to leave: $basename): " alias
-  target="$binpath/${alias:-$basename}"
-  mv "$tmp/$bin" "$target"
-  chmod +x "$target"
-  echo "Success!"
-  echo "Saved in: $target"
+    # install
+    basename=$(basename $bin)
+    read -p "Choose an alias (empty to leave: $basename): " alias
+    target="$binpath/${alias:-$basename}"
+    mv "$tmp/$bin" "$target"
+    chmod +x "$target"
+    echo "Success!"
+    echo "Saved in: $target"
+  else
+    echo "$1 is already installed"
+  fi
 }
 
 echo "ohmyzsh"
-if [ -d "$HOME/.oh-my-zsh" ]; then
+OMZ_DIR="$HOME/.oh-my-zsh"
+if [ -d "$OMZ_DIR" ]; then
   /bin/zsh -i -c 'omz update'
 else
   echo "Installing oh-my-zsh"
-  git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
+  git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git "$OMZ_DIR"
 fi
 
-OH_MY_ZSH_CUSTOM_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+omz_custom_dir="${ZSH_CUSTOM:-$OMZ_DIR/custom}"
 ## p10k
+P10K_DIR="$omz_custom_dir/themes/powerlevel10k"
 echo "p10k"
-if [ -d "$OH_MY_ZSH_CUSTOM_DIR/themes/powerlevel10k" ]; then
-  git -C "$OH_MY_ZSH_CUSTOM_DIR/themes/powerlevel10k" pull --rebase --force
+if [ -d "$P10K_DIR" ]; then
+  git -C "$P10K_DIR" pull --rebase --force
 else
   echo "Installing p10k"
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$OH_MY_ZSH_CUSTOM_DIR/themes/powerlevel10k"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
 fi
 
 ## zsh-autosuggestions
-ZSH_AUTOSUGGESTIONS_DIR="$OH_MY_ZSH_CUSTOM_DIR/plugins/zsh-autosuggestions"
+ZSH_AUTOSUGGESTIONS_DIR="$omz_custom_dir/plugins/zsh-autosuggestions"
 echo "zsh-autosuggestions"
 if [ -d "$ZSH_AUTOSUGGESTIONS_DIR" ]; then
   git -C "$ZSH_AUTOSUGGESTIONS_DIR" pull --rebase --force
@@ -88,14 +96,13 @@ else
 fi
 
 ## zsh-syntax-highlighting
-ZSH_SYNTAX_HIGHLIGHTING_DIR="$OH_MY_ZSH_CUSTOM_DIR/plugins/zsh-syntax-highlighting"
+ZSH_SYNTAX_HIGHLIGHTING_DIR="$omz_custom_dir/plugins/zsh-syntax-highlighting"
 echo "zsh-syntax-highlighting"
 if [ -d "$ZSH_SYNTAX_HIGHLIGHTING_DIR" ]; then
   git -C "$ZSH_SYNTAX_HIGHLIGHTING_DIR" pull --rebase --force
 else
   git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_SYNTAX_HIGHLIGHTING_DIR"
 fi
-
 
 ## fzf
 FZF_DIR="$HOME/.fzf"
@@ -105,16 +112,23 @@ if [ -d "$FZF_DIR" ]; then
 else
   git clone --depth 1 https://github.com/junegunn/fzf.git "$FZF_DIR"
 fi
-"$FZF_DIR/install" --all --key-bindings --completion --no-update-rc
 
-## fd
-echo "fd"
-install_from_github sharkdp/fd
+# START FROM HERE (PLATFORM SPECIFIC)
+OS="$(uname -s)"
+case "$OS" in
+  'Linux')
+    install_from_github fd sharkdp/fd
+    install_from_github fnm Schniz/fnm
+    install_from_github nvim neovim/neovim
+    install_from_github exa ogham/exa
+    ;;
+  'Darwin')
+    brew install fd fnm neovim exa
+    ;;
+  *)
+    echo "Unsupported OS"
+    exit 1
+    ;;
+esac
 
-## fnm
-echo "fnm"
-install_from_github Schniz/fnm
-
-## nvim
-echo "nvim"
-install_from_github neovim/neovim
+echo "Done!"
