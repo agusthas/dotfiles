bd3c1f19af23751f580702a241a5ce9fb9253e28
SusuMantan <suzumantan@gmail.com>
2022-08-03 20:40:44 +0700

    automatically install and sync packer

--- nvim/.config/nvim/lua/deps.lua
+++ nvim/.config/nvim/lua/deps.lua
@@ -1,3 +1,10 @@
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 vim.cmd [[packadd packer.nvim]]
 
 local has = function(x)
@@ -33,6 +40,10 @@ return require('packer').startup {
 
     -- Statusline
     use { "nvim-lualine/lualine.nvim", config = function() require('lualine').setup() end }
+
+    if packer_bootstrap then
+      require('packer').sync()
+    end
   end,
 
   config = {
