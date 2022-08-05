f6962893776daf6e6f35080a98483b83471cc66b
SusuMantan <suzumantan@gmail.com>
2022-08-05 13:21:36 +0700

    create bin directory so that stow doesnt create symlink dir

--- dotfiles.sh
+++ dotfiles.sh
@@ -6,6 +6,12 @@ stow_dotfiles() {
   stow -d "$HOME/dotfiles" -t ~ --verbose=2 "$@"
 }
 
+# Create initial bin directories
+# This is a workaround for the fact that stow SYMLINK the directory if it doesn't exist
+if [ ! -d "$HOME/bin" ]; then
+  mkdir -p "$HOME/bin"
+fi
+
 stow_dotfiles zsh p10k git nvim tmux bin
 
 case "$(uname -s)" in
