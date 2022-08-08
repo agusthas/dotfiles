cba43c77d8ea10804d2b126f6bc424eb6b82301b
SusuMantan <suzumantan@gmail.com>
2022-08-08 15:09:21 +0000

    done

--- app.sh
+++ app.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-YES="Yes"
-NO="No"
 APP_BIN_DIR=${APP_BIN_DIR:-$HOME/bin}
 
+# region Helpers
 __log() {
   echo -e "$1"
 }
@@ -23,28 +22,35 @@ __error() {
 __warn() {
   echo -e "[WARN] $1"
 }
+# endregion
 
 # START FROM HERE (PLATFORM SPECIFIC)
-if [[ -z "$skip_binaries" ]]; then
-  __log "Skip installing binaries?"
-  skip_binaries=$(echo -e "$YES\n$NO" | fzf --height 30 --reverse --no-info) || exit 1
-  __info "$skip_binaries\n"
-fi
 
-if ! [[ "$skip_binaries" = "$YES" ]]; then
+declare -Ar apps=(
+  [fd]="sharkdp/fd"
+  [fnm]="Schniz/fnm"
+  [exa]="ogham/exa"
+  [bat]="sharkdp/bat"
+  [gh]="cli/cli"
+)
+
+for key in ${!apps[@]}; do
+  printf "%s -> %s\n" "$key" "${apps[$key]}"
+done
+read -r -p "Do you want to install these apps? [y/N] " response
+
+if [[ $response =~ ^[Yy]$ ]]; then
   case "$(uname -s)" in
   'Linux')
-    downgit sharkdp/fd "$APP_BIN_DIR"
-    downgit Schniz/fnm "$APP_BIN_DIR"
-    downgit ogham/exa "$APP_BIN_DIR"
-    downgit sharkdp/bat "$APP_BIN_DIR"
-    downgit cli/cli "$APP_BIN_DIR"
+    for key in ${!apps[@]}; do
+      downgit ${apps[$key]} "${APP_BIN_DIR}"
+    done
     ;;
   'Darwin')
-    brew install bat fd fnm neovim exa gh
+    brew install ${!apps[@]}
     ;;
   *)
-    __log "Unsupported OS"
+    __error "Unsupported OS"
     exit 1
     ;;
   esac
@@ -53,7 +59,7 @@ fi
 # UNIVERSAL
 __log "UNIVERSAL"
 
-__completed "oh-my-zsh"
+__info "oh-my-zsh"
 OMZ_DIR="$HOME/.oh-my-zsh"
 if [[ ! -d "$OMZ_DIR" ]]; then
   git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git "$OMZ_DIR"
@@ -61,7 +67,7 @@ else
   echo "Already up to date."
 fi
 
-__completed "p10k"
+__info "p10k"
 omz_custom_dir="${ZSH_CUSTOM:-$OMZ_DIR/custom}"
 P10K_DIR="$omz_custom_dir/themes/powerlevel10k"
 if [ -d "$P10K_DIR" ]; then
@@ -70,7 +76,7 @@ else
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
 fi
 
-__completed "zsh-autosuggestions"
+__info "zsh-autosuggestions"
 ZSH_AUTOSUGGESTIONS_DIR="$omz_custom_dir/plugins/zsh-autosuggestions"
 if [ -d "$ZSH_AUTOSUGGESTIONS_DIR" ]; then
   git -C "$ZSH_AUTOSUGGESTIONS_DIR" pull --rebase --force
@@ -78,7 +84,7 @@ else
   git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "$ZSH_AUTOSUGGESTIONS_DIR"
 fi
 
-__completed "zsh-syntax-highlighting"
+__info "zsh-syntax-highlighting"
 ZSH_SYNTAX_HIGHLIGHTING_DIR="$omz_custom_dir/plugins/zsh-syntax-highlighting"
 if [ -d "$ZSH_SYNTAX_HIGHLIGHTING_DIR" ]; then
   git -C "$ZSH_SYNTAX_HIGHLIGHTING_DIR" pull --rebase --force
