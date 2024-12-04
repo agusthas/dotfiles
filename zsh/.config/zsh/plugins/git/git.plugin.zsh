b0b9550f694b6dda09722031f7b3b20026df5859
SusuMantan <suzumantan@gmail.com>
2024-12-04 22:18:28 +0700

    onprogress gitplugin

--- zsh/.config/zsh/plugins/git/git.plugin.zsh
+++ zsh/.config/zsh/plugins/git/git.plugin.zsh
@@ -30,48 +30,52 @@ function git_main_branch() {
   return 1
 }
 
-function grename() {
+function git_check_diff_commit() {
   command git rev-parse --is-inside-work-tree &>/dev/null || return
-  local old_branch_name new_branch_name
-
-  old_branch_name=$(git rev-parse --abbrev-ref HEAD)
-
-  if [[ -z "$1" ]]; then
-    echo "No new branch name provided"
-    return 1
-  fi
-
-  new_branch_name="$1"
-
-  echo "Renaming branch $old_branch_name to $new_branch_name"
-  git branch -m "$old_branch_name" "$new_branch_name"
-
-  # if user have --with-remote flag, replace the remote branch with the new branch
-  if [[ $2 == "--with-remote" ]]; then
-    upstream_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
-
-    # make sure upstream branch name is the same as old branch name
-    is_upstream_branch_name_same=$(echo "$upstream_branch" | grep -o "$old_branch_name")
-    if [ -z "$is_upstream_branch_name_same" ]; then
-      echo "Upstream branch name is not the same as the old branch name"
-      echo "Skipping pushing upstream branch"
-      echo "If you still want to rename the upstream branch, run this command:"
-      echo "  git push origin :$old_branch_name $new_branch_name"
-      echo "  git push -u origin $new_branch_name"
-      return 0
-    fi
-
-    echo
-    echo "Updating upstream branch $upstream_branch to $new_branch_name"
-
-    git push origin :"$old_branch_name" $new_branch_name
-    git push -u origin "$new_branch_name"
-  fi
-
-  echo
-  echo "Successfully renamed branch $old_branch_name to $new_branch_name"
+  git rev-list --left-right --pretty=oneline $1...$2
 }
 
+# function grename() {
+#   command git rev-parse --is-inside-work-tree &>/dev/null || return
+#   local old_branch_name new_branch_name
+
+#   old_branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+#   if [[ -z "$1" ]]; then
+#     echo "No new branch name provided"
+#     return 1
+#   fi
+
+#   new_branch_name="$1"
+
+#   echo "Renaming branch $old_branch_name to $new_branch_name"
+#   git branch -m "$old_branch_name" "$new_branch_name"
+
+#   # if user have --with-remote flag, replace the remote branch with the new branch
+#   if [[ $2 == "--with-remote" ]]; then
+#     upstream_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+
+#     # make sure upstream branch name is the same as old branch name
+#     is_upstream_branch_name_same=$(echo "$upstream_branch" | grep -o "$old_branch_name")
+#     if [ -z "$is_upstream_branch_name_same" ]; then
+#       echo "Upstream branch name is not the same as the old branch name"
+#       echo "Skipping pushing upstream branch"
+#       echo "If you still want to rename the upstream branch, run this command:"
+#       echo "  git push origin :$old_branch_name $new_branch_name"
+#       echo "  git push -u origin $new_branch_name"
+#       return 0
+#     fi
+
+#     echo
+#     echo "Updating upstream branch $upstream_branch to $new_branch_name"
+
+#     git push origin :"$old_branch_name" $new_branch_name
+#     git push -u origin "$new_branch_name"
+#   fi
+
+#   echo
+#   echo "Successfully renamed branch $old_branch_name to $new_branch_name"
+# }
 
 alias groot='cd "$(git rev-parse --show-toplevel || echo .)"'
 
@@ -104,14 +108,14 @@ alias gsw="git switch"
 alias gswc="git switch --create"
 alias gco="git checkout"
 
-alias gwt='git worktree'
-alias gwta='git worktree add'
-alias gwtls='git worktree list'
-alias gwtmv='git worktree move'
-alias gwtrm='git worktree remove'
+# alias gwt='git worktree'
+# alias gwta='git worktree add'
+# alias gwtls='git worktree list'
+# alias gwtmv='git worktree move'
+# alias gwtrm='git worktree remove'
 
 alias groll='git reset --hard origin/$(git_current_branch)'
 alias gclean="git reset --hard HEAD && git clean -df"
-alias gpristine='git reset --hard && git clean --force -dfx'
+alias gpristine='git reset --hard && git clean -dfx'
 
 alias ggzip='git archive --format zip --output /tmp/latest.zip HEAD'
